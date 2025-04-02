1c0130636c3dec3c1d2e3b3b4bc1742c79e04deb
M7mdful <ipadmohammed0@gmail.com>
2025-04-02 19:03:31 +0300

    orders + cart designed

--- cart.php
+++ cart.php
@@ -1,58 +1,48 @@
-<?php
-session_start();
-
-require './config/connection.php';
-
-
-?>
-
-
-
 <!DOCTYPE html>
 <html lang="en">
 
 <?php require('./template/header.php') ?>
-
 <?php if ($_SESSION['Role'] === "C") { ?>
-  <section>
-    <h1 class="text-center text-5xl py-4">cart</h1>
-
-    <div class="flex flex-col items-center px-24 gap-y-8" id="listOfTheCart">
-
-
-
-      <div class="flex flex-row justify-between items-center w-full bg-white p-4 rounded-lg">
-        <div class="w-12 h-12">
-          <img src="./assets/Logo.png" alt="burgerPic">
+    <div class="p-7 min-h-screen">
+        <div class=" flex flex-col gap-2.5">
+        <h1 class="text-2xl font-bold ml-11">Orders</h1>
+        <div class="flex flex-col gap-1.5 bg-gradient-to-br from-red-500 via-orange-400 to-yellow-300 p-2 rounded-2xl justify-center">
+            <div class="bg-white flex flex-col gap-2 p-1.5 rounded-2xl">
+                <table class="table-auto text-center">
+                    <thead class="border-b-2">
+                        <tr class="">
+                        <th>Order ID</th>
+                        <th>Number of Items</th>
+                        <th>Price</th>
+                        <th>Date</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr class="hover:bg-orange-300">
+                            <td>123123</td>
+                            <td>3</td>
+                            <td>500SAR</td>
+                            <td>2025/5/1</td>
+                        </tr>
+                        <tr class="hover:bg-orange-300">
+                            <td>142344</td>
+                            <td>5</td>
+                            <td>1000SAR</td>
+                            <td>2025/7/2</td>
+                            </tr>
+                        <tr class="hover:bg-orange-300">
+                            <td>238948</td>
+                            <td>2</td>
+                            <td>200SAR</td>
+                            <td>2025/9/1</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
-
-        <div class="flex-grow px-12 text-lg">
-          name of the burger
         </div>
-
-        <div class="flex items-center">
-          <p class="pr-4">quantity 1</p>
-          <!-- <input type="number" min="1" value="quantity"> -->
-          <input type="submit" value="Delete" class="p-3 rounded-lg bg-red-500 text-white" name="Delete">
-        </div>
-
-      </div>
-
-      <div id="totalCheckout">
-        <div>
-          <h1>Total: 23$</h1>
-          <input type="submit" value="checkout" class="bg-red-500 p-2 rounded-lg text-white">
-        </div>
-
-      </div>
     </div>
-  </section>
 <?php } else {
   header('Location:index.php');
 } ?>
-
-
-
 <?php require('./template/footer.php') ?>
-
-</html>
---
+++ orders.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php require('./template/header.php') ?>
+    <div class="p-7 min-h-screen">
+        <div class=" flex flex-col gap-2.5">
+        <h1 class="text-2xl font-bold ml-11">Orders</h1>
+        <div class="flex flex-col gap-1.5 bg-gradient-to-br from-red-500 via-orange-400 to-yellow-300 p-2 rounded-2xl justify-center">
+            <div class="bg-white flex flex-col gap-2 p-1.5 rounded-2xl">
+                <table class="table-auto text-center">
+                    <thead class="border-b-2">
+                        <tr class="">
+                        <th>Order ID</th>
+                        <th>Number of Items</th>
+                        <th>Price</th>
+                        <th>Date</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr class="hover:bg-orange-300">
+                            <td>123123</td>
+                            <td>3</td>
+                            <td>500SAR</td>
+                            <td>2025/5/1</td>
+                        </tr>
+                        <tr class="hover:bg-orange-300">
+                            <td>142344</td>
+                            <td>5</td>
+                            <td>1000SAR</td>
+                            <td>2025/7/2</td>
+                            </tr>
+                        <tr class="hover:bg-orange-300">
+                            <td>238948</td>
+                            <td>2</td>
+                            <td>200SAR</td>
+                            <td>2025/9/1</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+        </div>
+    </div>
+<?php require('./template/footer.php') ?>
