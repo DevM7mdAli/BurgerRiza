56b549192195a88795bab8a5bc9780fbe95eff02
DevM7mdAli <74423891+DevM7mdAli@users.noreply.github.com>
2025-04-12 13:03:58 +0300

    fixing footer and fixing the resturent template

--- template/footer.php
+++ template/footer.php
@@ -1,5 +1,5 @@
 <footer class="bg-black w-full">
-  <div class="flex flex-row text-2xl font-mono text-white p-5 justify-center">2024 Copyright <?php APPNAME ?></div>
+  <div class="flex flex-row text-2xl font-mono text-white p-5 justify-center">2024 Copyright <?php echo APPNAME ?></div>
 </footer>
 <!-- end of the footer -->
 </body>
